79d66464df9df23a2dcb84181c20599676ad23c7
Arman Amini <armanwithamini@gmail.com>
2024-08-17 23:24:31 -0700

    feat: :sparkles: add a notfound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import { CiWarning } from "react-icons/ci";
+import BaseButton from "@/components/Button";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <CiWarning className="w-20 h-20" />
+      <h1 className="text-2xl font-bold mb-4">Page Not Found</h1>
+      <p className="text-gray-600">
+        The page you are looking for does not exist. It might have been moved or
+        deleted.
+      </p>
+      <Link href="/">
+        <BaseButton fullWidth={false} mode="secondary" extraClasses="px-4 mt-2">
+          Home
+        </BaseButton>
+      </Link>
+    </div>
+  );
+}
