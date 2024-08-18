4e1b9e344495ee9035e50712982ba5ed43818901
Arman Amini <armanwithamini@gmail.com>
2024-08-18 12:04:14 -0700

    fix: :lipstick: product card

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -34,32 +34,28 @@ export const ThemeProvider = ({
   children,
   initialTheme = "dark",
 }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState<Theme | null>(null);
+  const [theme, setTheme] = useState<Theme>(initialTheme);
 
   useEffect(() => {
     // Get the theme from the cookie or use initialTheme
     const cookieTheme = nookies.get(null).theme as Theme;
-    setTheme(cookieTheme || initialTheme);
+    if (cookieTheme) {
+      setTheme(cookieTheme);
+    } else {
+      setTheme(initialTheme);
+    }
   }, [initialTheme]);
 
   useEffect(() => {
-    if (theme) {
-      // Apply the theme to the <html> element
-      document.documentElement.classList.remove("light", "dark");
-      document.documentElement.classList.add(theme);
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
 
-      // Save the preference in cookies
-      nookies.set(null, "theme", theme, { path: "/" });
-    }
+    nookies.set(null, "theme", theme, { path: "/" });
   }, [theme]);
 
   const toggleTheme = () => {
-    console.log(theme)
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
-
-  // If theme is still null, do not render children yet
-  if (!theme) return null;
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
